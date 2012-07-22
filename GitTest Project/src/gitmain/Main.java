3fdb5d18067c8ef5936f86cf680763152e279b9d
Michael Mattson <mijmattson@gmail.com>
2012-07-22 18:02:11 -0500

    Added print statement

--- GitTest Project/src/gitmain/Main.java
+++ GitTest Project/src/gitmain/Main.java
@@ -8,6 +8,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
+		System.out.println("Hello Again!");
 
 	}
 
